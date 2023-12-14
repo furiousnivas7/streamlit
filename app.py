0e5d635f1669ba986caf41c703423ff5318737c7
ashonasho <ashonmcmillan1234@gmail.com>
2023-12-14 23:42:00 +0530

    date

--- app.py
+++ app.py
@@ -1,2 +1,7 @@
-import streamlit as ashon
-ashon.write("vanakam ashon")
+import streamlit as st
+import datetime
+st.write("vanakam ashon")
+firstname=st.text_input("firstname","enter your first name")
+secondname=st.text_input("secondname","enter your second name")
+d = st.date_input("When's your birthday", datetime.date(2019, 7, 6))
+# st.write('Your birthday is:', d)
