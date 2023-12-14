0b01f54f8b14f0a76488632d1207a53555b3cc2b
ashonasho <ashonmcmillan1234@gmail.com>
2023-12-15 00:33:12 +0530

    added new data file handling

--- app.py
+++ app.py
@@ -1,8 +1,28 @@
 import streamlit as st
 import datetime
 import pandas as pd
+import json
+import os
+
 
 st.write("Vanakam Ashon")
+json_file_path = 'data.json'
+
+# Function to load data from JSON file
+def load_data(file_path):
+    if os.path.exists(file_path):
+        with open(file_path, 'r') as file:
+            return json.load(file)
+    return []
+
+# Function to save data to JSON file
+def save_data(file_path, data):
+    with open(file_path, 'w') as file:
+        json.dump(data, file, indent=4, default=str)
+
+        
+# Load existing data
+data = load_data(json_file_path)
 
 # Input fields
 firstname = st.text_input("Firstname", placeholder="Enter your first name", max_chars=10)
@@ -12,11 +32,14 @@ button = st.button('Submit')
 # Button to submit data
 if button:
     # Creating a DataFrame to display the data in a table format
-    data = {
-        "Firstname": [firstname],
-        "Secondname": [secondname],
-        "Birthday": [birthday]
+    new_entry = {
+        "Firstname": firstname,
+        "Secondname": secondname,
+        "Birthday": birthday
     }
+    data.append(new_entry)
+
+    save_data(json_file_path, data)
     df = pd.DataFrame(data)
     
     # Display the DataFrame
