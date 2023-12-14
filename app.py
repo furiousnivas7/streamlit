11f3ea58c7ff523a30dba65cc91952cc671c8ab6
ashonasho <ashonmcmillan1234@gmail.com>
2023-12-15 00:40:34 +0530

    download button

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ import pandas as pd
 import json
 import os
 
-
 st.write("Vanakam Ashon")
 json_file_path = 'data.json'
 
@@ -20,7 +19,6 @@ def save_data(file_path, data):
     with open(file_path, 'w') as file:
         json.dump(data, file, indent=4, default=str)
 
-        
 # Load existing data
 data = load_data(json_file_path)
 
@@ -28,19 +26,33 @@ data = load_data(json_file_path)
 firstname = st.text_input("Firstname", placeholder="Enter your first name", max_chars=10)
 secondname = st.text_input("Secondname", placeholder="Enter your second name", max_chars=10)
 birthday = st.date_input("When's your birthday", datetime.date(2019, 7, 6))
+fees_paid = st.radio("Fees Paid", ["Yes", "No"])
+last_class_date = st.date_input("Last Class Date", datetime.date.today())
+exam_marks = st.number_input("Exam Marks", min_value=0, max_value=100, value=0)
 button = st.button('Submit')
+
 # Button to submit data
 if button:
-    # Creating a DataFrame to display the data in a table format
     new_entry = {
         "Firstname": firstname,
         "Secondname": secondname,
-        "Birthday": birthday
+        "Birthday": birthday,
+        "Fees Paid": fees_paid,
+        "Last Class Date": last_class_date,
+        "Exam Marks": exam_marks
     }
     data.append(new_entry)
-
     save_data(json_file_path, data)
     df = pd.DataFrame(data)
     
     # Display the DataFrame
     st.table(df)
+
+    # Display bar chart of exam marks
+    if len(data) > 0:
+        st.bar_chart(df["Exam Marks"])
+
+# Button to download JSON file
+if st.button('Download JSON Data'):
+    with open(json_file_path, "r") as file:
+        st.download_button(label="Download Data as JSON", data=file, file_name='student_data.json', mime='application/json')
