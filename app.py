935c82ef01c674bfe6b26405fe4b3d759fec3365
ashonasho <ashonmcmillan1234@gmail.com>
2023-12-14 23:48:52 +0530

    create place holder

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import datetime
 st.write("vanakam ashon")
-firstname=st.text_input("firstname","enter your first name")
-secondname=st.text_input("secondname","enter your second name")
+firstname=st.text_input("firstname",placeholder="enter your first name")
+secondname=st.text_input("secondname",placeholder="enter your second name")
 d = st.date_input("When's your birthday", datetime.date(2019, 7, 6))
 # st.write('Your birthday is:', d)
