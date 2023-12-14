f197466df118695f5a5099b4a8f05dbb005afc2f
ashonasho <ashonmcmillan1234@gmail.com>
2023-12-15 00:13:36 +0530

    button

--- app.py
+++ app.py
@@ -8,9 +8,9 @@ st.write("Vanakam Ashon")
 firstname = st.text_input("Firstname", placeholder="Enter your first name", max_chars=10)
 secondname = st.text_input("Secondname", placeholder="Enter your second name", max_chars=10)
 birthday = st.date_input("When's your birthday", datetime.date(2019, 7, 6))
-
+button = st.button('Submit')
 # Button to submit data
-if st.button('Submit'):
+if button:
     # Creating a DataFrame to display the data in a table format
     data = {
         "Firstname": [firstname],
